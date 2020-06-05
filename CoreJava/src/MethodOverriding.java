2b6a6e191ba4f720762fe202a35e7d7029a14814
Dilip <Dilip@DESKTOP-RDQKB5U>
2020-06-06 01:28:42 +0530

    commit

--- CoreJava/src/MethodOverriding.java
+++ CoreJava/src/MethodOverriding.java
@@ -1,27 +1,27 @@
 
-//class P{
-//	void show() {
-//		System.out.println("In P");
-//	}
-//	
-//}
-//
-//class Q extends P{
-//	@Override               //this annotation helps to avoid run time error if we somehow write say show1() instead of show here
-//	void show() {
-//		System.out.println("In Q");
-//	}
-//}
-//
-//
-//class R extends P{
-//	
-//	@Override
-//	void show() {
-//		super.show();          //with this super keyword, we can call  super class show() also
-//		System.out.println("In R");
-//	}
-//}
+class P{
+	void show() {
+		System.out.println("In P");
+	}
+	
+}
+
+class Q extends P{
+	@Override               //this annotation helps to avoid run time error if we somehow write say show1() instead of show here
+	void show() {
+		System.out.println("In Q");
+	}
+}
+
+
+class R extends P{
+	
+	@Override
+	void show() {
+		super.show();          //with this super keyword, we can call  super class show() also
+		System.out.println("In R");
+	}
+}
 //
 //public class MethodOverriding {
 //
